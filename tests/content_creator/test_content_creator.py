ea980991733a75eceefbff52010ac882cf803d3e
Jan Cichomski <yancichomski@gmail.com>
2023-11-29 16:52:15 +0100

    Improve docker compose and dockerfiles, add tests

---
+++ tests/content_creator/test_content_creator.py
@@ -0,0 +1,8 @@
+from radio.content_creator.content_creator import ContentCreator
+from radio.content_creator.content_creator_instance import create_content_creator
+
+
+def test_create_content_creator():
+    content_creator = create_content_creator()
+    assert isinstance(content_creator, ContentCreator)
+    assert len(content_creator.channels) > 0
