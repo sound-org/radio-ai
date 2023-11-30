372069bccacbee5f2a66659a184724e497a844a2
Jan Cichomski <yancichomski@gmail.com>
2023-11-30 22:24:49 +0100

    save

--- radio/tests/conftest.py
+++ radio/tests/conftest.py
@@ -2,10 +2,11 @@ import os
 
 import dotenv
 
-result = dotenv.load_dotenv("../.env")
+result = dotenv.load_dotenv("radio/.env")
 if result is False:
     cwd = os.getcwd()
     print("Current working directory: " + cwd)
-    raise Exception("Failed to load .env file")
+    raise Exception("Failed to load .env file" + cwd)
+
 
 RUN_EXTERNAL_API_TESTS = True
